e5ce00eccffed76ede2d7850b3b98d648599b5c3
notnilo <danilo.vergel.bermudez@gmail.com>
2024-11-17 16:42:38 -0500

    controllers1

---
+++ src/core/controllers/TransactionController.java
@@ -0,0 +1,14 @@
+package core.controllers;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author notnilo
+ */
+public class TransactionController {
+    
+}
--- src/core/models/storage/Storage.java
+++ src/core/models/storage/Storage.java
@@ -27,4 +27,40 @@ public class Storage {
         this.accounts = new ArrayList<>();
         this.transactions = new ArrayList<>();
     }
+    
+    public static Storage getInstance() {
+        if (instance == null) {
+            instance = new Storage();
+        }
+        return instance;
+    }
+    
+    public boolean addUser(User user) {
+        for (User u : this.users) {
+            if (u.getId() == u.getId()) {
+                return false;
+            }
+        }
+        this.users.add(user);
+        return true;
+    }
+    
+    public Person getPerson(int id) {
+        for (Person person : this.persons) {
+            if (person.getId() == id) {
+                return person;
+            }
+        }
+        return null;
+    }
+    
+    public boolean delPerson(int id) {
+        for (Person person : this.persons) {
+            if (person.getId() == id) {
+                this.persons.remove(person);
+                return true;
+            }
+        }
+        return false;
+    }
 }
